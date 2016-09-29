684652bdff0b12f66469cd4f2cc753adee72c4a2
“7Elias” <o.jingyao@gmail.comssh-keygen -t rsa -C o.jingyao@gmail.comcat>
2016-09-29 17:45:28 +0800

    try

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module AirBnb
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.active_job.queue_adapter = :sidekiq
+    config.assets.initialize_on_precompile = false
   end
   # config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
